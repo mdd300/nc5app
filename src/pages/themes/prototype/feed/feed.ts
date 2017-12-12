03bec4f564e9acfa65f8e007ad83614e727fb7a9
Danillo Alves <danillo.alves@uniquechic.com.br>
2017-12-12 17:03:42 -0200

    End of page fee

--- src/pages/themes/prototype/feed/feed.ts
+++ src/pages/themes/prototype/feed/feed.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {Constants} from "../../../../config/Constants";
+import * as $ from "jquery";
 
 /**
  * Generated class for the FeedPage page.
@@ -11,18 +12,70 @@ import {Constants} from "../../../../config/Constants";
 
 @IonicPage()
 @Component({
-  selector: 'page-feed',
-  templateUrl: 'feed.html',
+    selector: 'page-feed',
+    templateUrl: 'feed.html',
 })
 export class FeedPage {
 
-  public constants = Constants;
+    public constants = Constants;
 
-  constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams
-  ) {}
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams) {
+    }
 
 
+    /**
+     * Estrutura Padrão e Base de como os dados do feed devem ser retornados */
+    public feed = [
+        {type: 1, company: 'Unique', image: "post1.jpg", products: null},
+        {type: 1, company: 'Unieke', image: "unique.jpg", products: null},
+        {type: 1, company: 'CHLOE', image: null, products: null},
+        {type: 2, company: 'Marisa', image: 'roupa5.jpg', products: null},
+        {
+            type: 3, company: 'Zara', image: 'wallpaper.jpg', products:
+            [
+                { group: [
+                    {prod_image: 'roupa5.jpg'},
+                    {prod_image: 'roupa1.jpg'},
+                    {prod_image: 'roupa1.png'},
+                ]},
+                { group: [
+                    {prod_image: 'roupa5.jpg'},
+                    {prod_image: 'roupa2.png'},
+                    {prod_image: 'roupa4.jpg'}
+                ]}
+            ]/* Fim do products */
+        },
+        {
+            type: 3, company: 'Brooksfield', image: null, products:
+            [
+                { group: [
+                    {prod_image: 'roupa1.jpg'},
+                    {prod_image: 'roupa2.png'},
+                    {prod_image: 'roupa3.jpg'},
+                ]},
+                { group: [
+                    {prod_image: 'roupa1.png'},
+                    {prod_image: 'roupa2.png'},
+                    {prod_image: 'roupa1.png'}
+                ]},
+                { group: [
+                    {prod_image: 'roupa1.jpg'},
+                    {prod_image: 'roupa2.png'},
+                ]},
+            ]/* Fim do products */
+        },
+        {type: 1, company: 'WebCloset', image: null, products: null}
+    ]
+    /* Fim da estrutura base de como o feed deve ser retornado */
+
+
+    @ViewChild('filterCaret') filterCaret:any;
+    public changeFilter = (( event ) => {
+        let $caret = $(this.filterCaret.nativeElement);
+        let $filter = $( event.path[0] );
+        let dimensions = { w: $filter.width(), x: $filter.position().left }
+        $caret.css({ width: dimensions.w+'px', left: dimensions.x + 'px' });
+    });
 
 }
