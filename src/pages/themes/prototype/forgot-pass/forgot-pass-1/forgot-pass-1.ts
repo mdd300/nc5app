4952fd9bbf231dd1d1dc596c2e4eb82081d9f5bf
Danillo Alves <danillo.alves@uniquechic.com.br>
2017-12-15 12:38:06 -0200

    Recover Pass - Validade Email

--- src/pages/themes/prototype/forgot-pass/forgot-pass-1/forgot-pass-1.ts
+++ src/pages/themes/prototype/forgot-pass/forgot-pass-1/forgot-pass-1.ts
@@ -15,14 +15,22 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class ForgotPass_1Page {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
-    }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
+    public valid_email:boolean;
     /**
      * Função utilizada para "voltar" uma pagina do aplicativo */
     public backPage = (() => {
         /* Remove uma página do controller de navegação */
         this.navCtrl.pop();
     }); /* Fim da função de backPage */
+
+
+    /**
+     * Verifica se o email é valido para prosseguir */
+    public validateEmail = (( email:string ) :void => {
+        let regex = new RegExp(/([a-zA-Z0-9\._\-]{2})+@+([a-zA-Z0-9]{2,20})+\.+[a-z]{2}/);
+        this.valid_email = regex.test( email );
+    });
 
 }
