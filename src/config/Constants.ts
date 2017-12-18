86c4890d20424d06f36f9b3df0a91b1f6c3ed304
Jefferson Sousa <jefferson.sousa@uniquechic.com.br>
2017-12-18 18:58:06 -0200

    constante online

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -4,7 +4,7 @@ export const Constants = {
 
    //'api_path': 'http://192.168.0.251/fashon/qrgo/',
     //'api_path': 'http://localhost/fashon/qrgo/',
-    'api_path': 'http://ec2-184-72-119-250.compute-1.amazonaws.com',
+    'api_path': 'http://ec2-184-72-119-250.compute-1.amazonaws.com/qrgo/',
 
     'src_img': './assets/themes/prototype/img/',
     'src_svg': './assets/themes/prototype/img/svg/',
